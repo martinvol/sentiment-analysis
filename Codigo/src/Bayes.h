5da355cfca77f56632f491935710dc0c5e2f2c24
Martín Volpe <martin.volpe@gmail.com>
2015-05-31 17:53:25 -0300

    Regex added.

---
+++ Codigo/src/Bayes.h
@@ -0,0 +1,23 @@
+#ifndef BAYES_H_
+#define BAYES_H_
+
+#include <vector>
+#include <cstring>
+
+class Bayes {
+private:
+
+
+public:
+	Bayes();
+
+	virtual ~Bayes();
+	
+	// entrenar, recibimos oraciones. Oraciones entiendo como un string que representa una oracion
+	// y si es positivo o negativo
+	// void entrenar(int tag, std::vector<string*>* features);
+	// evaluar, recibimos lista de oraciones.
+
+};
+
+#endif /* BAYES_H_ */
